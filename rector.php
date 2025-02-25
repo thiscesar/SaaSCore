ea7e5ef55d5854773ed2bac4ad8670f1179aa130
thiscesar <cesarldsousa@gmail.com>
2025-02-25 00:30:09 -0300

    Chore: run pint

--- rector.php
+++ rector.php
@@ -1,21 +1,23 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/database',
         __DIR__ . '/tests',
+        __DIR__ . '/routes',
     ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true
+    )
+    ->withPhpSets();
